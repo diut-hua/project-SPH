d0465e20c59f6ec51cd2b626fa139b32ab499f00
diut <1029296449@qq.com>
2023-02-08 22:46:19 +0800

    modified:   app/src/api/index.js 	modified:   app/src/main.js 	new file:   app/src/pages/Search/SearchSelector/SearchSelector.vue 	new file:   app/src/pages/Search/SearchSelector/images/phone01.png 	new file:   app/src/pages/Search/SearchSelector/images/phone02.png 	new file:   app/src/pages/Search/SearchSelector/images/phone06.png 	new file:   app/src/pages/Search/SearchSelector/images/phone07.png 	new file:   app/src/pages/Search/SearchSelector/images/phone08.png 	new file:   app/src/pages/Search/SearchSelector/images/phone09.png 	new file:   app/src/pages/Search/SearchSelector/images/phone10.png 	new file:   app/src/pages/Search/SearchSelector/images/phone11.png 	new file:   app/src/pages/Search/SearchSelector/images/phone12.png 	new file:   app/src/pages/Search/SearchSelector/images/phone14.png 	new file:   app/src/pages/Search/images/like_01.png 	new file:   app/src/pages/Search/images/like_02.png 	new file:   app/src/pages/Search/images/like_03.png 	new file:   app/src/pages/Search/images/like_04.png 	new file:   app/src/pages/Search/images/mobile01.png 	new file:   app/src/pages/Search/images/mobile02.png 	new file:   app/src/pages/Search/images/mobile03.png 	new file:   app/src/pages/Search/images/mobile04.png 	new file:   app/src/pages/Search/images/mobile05.png 	new file:   app/src/pages/Search/images/mobile06.png 	modified:   app/src/pages/Search/index.vue 	modified:   app/src/store/search/index.js

--- app/src/api/index.js
+++ app/src/api/index.js
@@ -14,3 +14,20 @@ export const reqGetBannerList = () => mockRequests.get('/banner')
 
 //获取floor
 export const reqGetFloorList = () => mockRequests.get('/floor')
+
+//获取搜索模块数据 地址：/api/list  请求方式：post  参数：需要带参数
+/*
+{
+  "category3Id": "61",
+  "categoryName": "手机",
+  "keyword": "小米",
+  "order": "1:desc",
+  "pageNo": 1,
+  "pageSize": 10,
+  "props": ["1:1700-2799:价格", "2:6.65-6.74英寸:屏幕尺寸"],
+  "trademark": "4:小米"
+}
+*/
+//当前这个函数需不需要接受外部传递参数
+//当前这个接口(获取搜索模块的数据)，给服务器传递的参数【至少是一个空对象】
+export const reqGetSearchInfo = (params) => requests({ url: '/list', method: 'post', data: params })
--- app/src/main.js
+++ app/src/main.js
@@ -18,6 +18,8 @@ import store from '@/store';
 
 //引入swiper样式
 import 'swiper/css/swiper.css';
+
+
 new Vue({
   render: h => h(App),
   //注册路由: 底下的写法KV一致省略V[router小写的]
